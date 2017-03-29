cb18f93abac9cc7c990c84f18239088f3541d382
Guillaume Leroi <leroi.g@gmail.com>
2017-03-30 00:07:33 +0200

    ui: a bit sexier

--- client/index.tsx
+++ client/index.tsx
@@ -1,10 +1,83 @@
 import * as React from "react";
 import * as Dom from "react-dom";
 
-class Page extends React.Component<any, any> {
+
+class Card extends React.Component<any, any> {
     render() {
-        return (<h1>Team Poker</h1>);
+        var cardStyle: React.CSSProperties = {
+            width: 100,
+            height: 150,
+            textAlign: "center",
+            verticalAlign: "middle",
+            border: "1px solid #AAAAAA",
+            borderRadius: 5,
+            margin: 5
+        }
+
+        var cardTextStyle: React.CSSProperties = {
+            verticalAlign: "middle",
+            marginTop: 60,
+            fontSize: "24pt",
+            fontWeight: "bold",
+            fontFamily: "sans-serif",
+            color: "#333333"
+        }
+
+        var { children, ...others} = this.props;
+
+        return (
+            <div style={cardStyle} {...others}><div style={cardTextStyle}>{children}</div></div>
+        )
     }
 }
 
-Dom.render(<Page/>, document.getElementById("main-container"));
+class Main extends React.Component<any, any> {
+    render() {
+
+        var columnsStyle: React.CSSProperties = {
+            display: "flex",
+            flexDirection: "row",
+            flexWrap: "nowrap"
+        }
+
+        var cardsStyle: React.CSSProperties = {
+            display: "flex",
+            flexDirection: "row",
+            flexWrap: "wrap"
+        }
+
+        var playersStyle: React.CSSProperties = {
+            width: 200
+        }
+
+
+        return (<div>
+            <h1>Team Poker</h1>
+
+            <div style={columnsStyle}>
+                <div style={cardsStyle}>
+
+                    <Card>1</Card>
+                    <Card>2</Card>
+                    <Card>3</Card>
+                    <Card>5</Card>
+                    <Card>8</Card>
+                    <Card>13</Card>
+                    <Card>21</Card>
+                    <Card><span className="fa fa-coffee"></span></Card>
+                    <Card>?</Card>
+
+                </div>
+
+                <div style={playersStyle}>
+                    <ul>
+                        <li>Joueur 1</li>
+                        <li>Joueur 2</li>
+                    </ul>
+                </div>
+            </div>
+        </div>);
+    }
+}
+
+Dom.render(<Main />, document.getElementById("main-container"));
--- public/index.js
+++ public/index.js
@@ -8,18 +8,94 @@ var __extends = (this && this.__extends) || (function () {
         d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
     };
 })();
+var __assign = (this && this.__assign) || Object.assign || function(t) {
+    for (var s, i = 1, n = arguments.length; i < n; i++) {
+        s = arguments[i];
+        for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+            t[p] = s[p];
+    }
+    return t;
+};
+var __rest = (this && this.__rest) || function (s, e) {
+    var t = {};
+    for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p) && e.indexOf(p) < 0)
+        t[p] = s[p];
+    if (s != null && typeof Object.getOwnPropertySymbols === "function")
+        for (var i = 0, p = Object.getOwnPropertySymbols(s); i < p.length; i++) if (e.indexOf(p[i]) < 0)
+            t[p[i]] = s[p[i]];
+    return t;
+};
 define("index", ["require", "exports", "react", "react-dom"], function (require, exports, React, Dom) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
-    var Page = (function (_super) {
-        __extends(Page, _super);
-        function Page() {
+    var Card = (function (_super) {
+        __extends(Card, _super);
+        function Card() {
             return _super !== null && _super.apply(this, arguments) || this;
         }
-        Page.prototype.render = function () {
-            return (React.createElement("h1", null, "Team Poker"));
+        Card.prototype.render = function () {
+            var cardStyle = {
+                width: 100,
+                height: 150,
+                textAlign: "center",
+                verticalAlign: "middle",
+                border: "1px solid #AAAAAA",
+                borderRadius: 5,
+                margin: 5
+            };
+            var cardTextStyle = {
+                verticalAlign: "middle",
+                marginTop: 60,
+                fontSize: "24pt",
+                fontWeight: "bold",
+                fontFamily: "sans-serif",
+                color: "#333333"
+            };
+            var _a = this.props, children = _a.children, others = __rest(_a, ["children"]);
+            return (React.createElement("div", __assign({ style: cardStyle }, others),
+                React.createElement("div", { style: cardTextStyle }, children)));
         };
-        return Page;
+        return Card;
     }(React.Component));
-    Dom.render(React.createElement(Page, null), document.getElementById("main-container"));
+    var Main = (function (_super) {
+        __extends(Main, _super);
+        function Main() {
+            return _super !== null && _super.apply(this, arguments) || this;
+        }
+        Main.prototype.render = function () {
+            var columnsStyle = {
+                display: "flex",
+                flexDirection: "row",
+                flexWrap: "nowrap"
+            };
+            var cardsStyle = {
+                display: "flex",
+                flexDirection: "row",
+                flexWrap: "wrap"
+            };
+            var playersStyle = {
+                width: 200
+            };
+            return (React.createElement("div", null,
+                React.createElement("h1", null, "Team Poker"),
+                React.createElement("div", { style: columnsStyle },
+                    React.createElement("div", { style: cardsStyle },
+                        React.createElement(Card, null, "1"),
+                        React.createElement(Card, null, "2"),
+                        React.createElement(Card, null, "3"),
+                        React.createElement(Card, null, "5"),
+                        React.createElement(Card, null, "8"),
+                        React.createElement(Card, null, "13"),
+                        React.createElement(Card, null, "21"),
+                        React.createElement(Card, null,
+                            React.createElement("span", { className: "fa fa-coffee" })),
+                        React.createElement(Card, null, "?")),
+                    React.createElement("div", { style: playersStyle },
+                        React.createElement("ul", null,
+                            React.createElement("li", null, "Joueur 1"),
+                            React.createElement("li", null, "Joueur 2"))))));
+        };
+        return Main;
+    }(React.Component));
+    Dom.render(React.createElement(Main, null), document.getElementById("main-container"));
 });
